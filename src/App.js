d2925a8afa6ebe1ffb471d5d2f67262bbe6ac03c
Evgeny Belyakov <bfjeka101@gmail.com>
2022-05-27 09:12:10 +0300

    add html and split for components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import './App.css';
+import { Categories } from './components/Categories/Categories';
+import { Header } from './components/Header/Header';
+import { PizzaBlock } from './components/PizzaBlock/PizzaBlock';
+import { Sort } from './components/Sort/Sort';
+import './scss/app.scss';
 
 function App() {
-    return <div className="App">hi all</div>;
+    return (
+        <div className="App">
+            <div class="wrapper">
+                <Header />
+                <div class="content">
+                    <div class="container">
+                        <div class="content__top">
+                            <Categories />
+                            <Sort />
+                        </div>
+                        <h2 class="content__title">Все пиццы</h2>
+                        <div class="content__items">
+                            <PizzaBlock title={'бургер-пицца'} price={300} />
+                            <PizzaBlock title={'4 сыра'} price={450} />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
