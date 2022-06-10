38ebda417776407b40cd47da9e30c4e5d81a7170
Evgeny Belyakov <bfjeka101@gmail.com>
2022-06-10 12:44:57 +0300

    add: component Sort - add functional hide/open for button sort

--- src/components/Sort/Sort.jsx
+++ src/components/Sort/Sort.jsx
@@ -14,15 +14,27 @@ export const sortList = [
 export const Sort = () => {
     const dispatch = useDispatch();
     const sort = useSelector((state) => state.filter.sort);
-
+    const sortRef = React.useRef();
     const [open, setOpen] = React.useState(false);
 
     const onClickListItem = (obj) => {
         dispatch(setSort(obj));
         setOpen(false);
     };
+
+    React.useEffect(() => {
+        const hadleClickOutside = (event) => {
+            if (!event.path.includes(sortRef.current)) {
+                setOpen(false);
+            }
+        };
+        document.body.addEventListener('click', hadleClickOutside);
+
+        return () => document.body.removeEventListener('click', hadleClickOutside);
+    }, []);
+
     return (
-        <div className="sort">
+        <div ref={sortRef} className="sort">
             <div className="sort__label">
                 <svg
                     width="10"
